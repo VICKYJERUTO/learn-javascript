7397b0b74e47bf04689e1febceb3d3466cd28a04
vicky jeruto <vickyjeruto14@gmail.com>
2023-06-09 16:32:05 -0800

    switch,ifelse

---
+++ lesson4/ifelse.js
@@ -0,0 +1,19 @@
+if(hour>18){
+    greetings="goodday";
+}else{
+    greetings="good evening";
+
+}
+
+
+
+if(hour<10)
+{
+    greeting="goodmorning";
+}Elseif(hour>20)
+{
+    greeting="goodday";
+Else(hour<10)
+    greeting="goodevening";
+}
+ 
